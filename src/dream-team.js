c649dabfd344a2154704f198697b492e766d8ef7
kalinskiy <kalinskiy.sasha@gmail.com>
2021-03-14 23:53:05 +0300

    fix: added new functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) return false;
+    if (members.length) {
+        let arr2 = members.map((element) => {
+            if (typeof element === "string") {
+                return element.trim()[0].toUpperCase()
+            }
+        });
+        return arr2.sort().join("");
+
+
+    }
 };
+
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if(typeof str !== 'string') {
+        str = String(str)
+    }
+    options.repeatTimes = options.repeatTimes || 1;
+    options.separator = options.separator || '+';
+
+    if(options.hasOwnProperty('addition')) {
+        if(typeof options.addition !== 'string') {
+            options.addition = options.addition + '';
+        }
+        options.additionRepeatTimes = options.additionRepeatTimes || 1;
+        options.additionSeparator = options.additionSeparator || '|'
+
+    } else {
+        options.addition = ''
+    }
+
+    const separator = options.addition !== '' ?
+        new Array(options.additionRepeatTimes)
+            .fill(options.addition)
+            .join(options.additionSeparator) :
+        ''
+
+    return new Array(options.repeatTimes)
+        .fill(str + separator)
+        .join(options.separator)
 };
-  
